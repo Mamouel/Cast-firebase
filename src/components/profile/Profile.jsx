9dcdbf0f5f8a73175d975f5e1e45a58249ff5f88
mamouel <e-dodier@hotmail.fr>
2019-02-02 16:01:06 -0500

    profile page now render different component if it is currentUser profile or otherUser profile

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -6,37 +6,92 @@ import { Redirect, Link } from 'react-router-dom';
 import StorySummary from '../stories/StorySummary';
 
 import '../../style/components/profile/profile.scss';
+import LoadingAnimation from '../layout/LoadingAnimation';
 
 class Profile extends Component {
 
-  getUserStories = (stories, auth) => {
+
+  getUserStories = (stories, id) => {
     let currentList = [];
     let newList = [];
-
-    if (stories && auth.uid) {
+    if (stories && id) {
       currentList = stories;
       newList = currentList.filter(story => {
-        if(story.authorId === auth.uid) {
+        if(story.authorId === id) {
           return story
         } else {
           return null
         }
       });
-
     } else {
       newList = stories;
     }
     return newList;
-
   }
 
-  render() {
 
-    const { stories, auth, profile } = this.props
+  getTargetedUser = (users, id) => { 
+    let usersList = [];
+    let targetedUser = [];
+    let targetedUserId = id;
+    if(users && targetedUserId) {
+      usersList = users;
+      targetedUser = usersList.filter(user => {
+        if(user.id === targetedUserId) return user;
+        return null;
+      });
+    } else {
+      targetedUser = [];
+    }
+    return targetedUser;
+  }
+
+  render(props) {
+
+    const { stories, auth, profile, users } = this.props;
+    const targetedUserId = this.props.match.params.id;
+    const currentUserId = auth.uid;
+
     if (!auth.uid) return <Redirect to='/signin'/>
     if(!stories) return <Redirect to='/'/>
-    const userStories = this.getUserStories(stories, auth);
-    if(userStories.length === 0) { 
+    const userStories = this.getUserStories(stories, auth.uid);
+
+    let targetedUser;
+    if(targetedUserId !== currentUserId) {
+      targetedUser = this.getTargetedUser(users, targetedUserId);
+      const targetedUserInfos = targetedUser[0];
+      
+      console.log(targetedUserInfos)
+      if(targetedUserInfos) {
+        const userTargetedStories = this.getUserStories(stories, targetedUserInfos.id);
+        return (
+          <div className='profile-container'>
+            <div className='profile-banner'>
+              <div className='profile-infos'>
+                <div>{ targetedUserInfos.firstName } { targetedUserInfos.lastName }</div>
+              </div>
+            </div>
+            <div className='profile-stories-title'>
+              <p>{ targetedUserInfos.firstName }'s Stories </p>
+            </div>
+            <div className='profile-stories-emptylist'>
+              {userTargetedStories && userTargetedStories.map(story => {
+                return (
+                  <Link className='story-link' to={'/story/' + story.id} key={story.id}>
+                    <StorySummary story={story} />
+                  </Link>
+                )
+              })}
+            </div>
+          </div>
+        )
+      } else {
+        return <LoadingAnimation />
+      }
+    } 
+
+
+    if(userStories.length === 0 && profile) { 
       return (
         <div className='profile-container'>
           <div className='profile-banner'>
